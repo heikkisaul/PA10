6ad63ba97330b2d583f018b35e7361eea0782e74
Heikki <heikki.saul@gmail.com>
2018-10-22 16:47:00 +0900

    Line drawing works, started work on circle drawing

--- pa10_controller/pa10/src/joint_state_pub.cpp
+++ pa10_controller/pa10/src/joint_state_pub.cpp
@@ -42,6 +42,7 @@ private:
     double Interpolate(std::vector<double> coeff_vector, double cur_time);
 
     std::vector<double> LinePathGenerator(std::vector<double> cartesian_vector_0, std::vector<double> cartesian_vector_f, double movement_dur, double ticks, std::vector<std::vector<double>> transformation_mat);
+    std::vector<double> CirclePathGenerator(std::vector<double> cartesian_vector_0, std::vector<double> cartesian_vector_d, double movement_dur, double ticks, std::vector<std::vector<double>> transformation_mat);
     double findCur5thOrderVal(double ticks, double duration_ticks, double x_0, double x_f);
 
     void MoveJoint(std::vector<double> theta_coeff_matrix);
@@ -51,7 +52,7 @@ private:
     bool ReachGoalFlag; //true:Reached goal, false: Not reached goal
     double ticks;
 
-    double movement_dur = 100;
+    double movement_dur = 50;
 
     std::vector<double> initial_angle_vector;
     //std::vector<std::vector<double>> theta_coeff_matrix;
@@ -71,16 +72,11 @@ PA10Controller::PA10Controller() {
     joint_pub = node.advertise<sensor_msgs::JointState>("/pa10/joint_states", CLOCK);
     initial_angle_vector = std::vector<double>({10, 10, 0, 10, 10, 10, 10});
 
-    //start_cartesian_vector = {272.561, 50.203, 1271.901};
-    delta_cartesian_vector = {0, 0, 0};
-    //delta_cartesian_vector = {100, 200, -200};
+    delta_cartesian_vector = {100, 200, -800};
 
-    //final_cartesian_vector = {100, 100, 1000, 0, 0, 0};
-    //final_cartesian_vector = {600, 400, 700, -M_PI/2, -M_PI/2, 0};
 }
 
 void PA10Controller::initJointState(sensor_msgs::JointState *joint_state)
-
 {
     joint_state->name.resize(9);
     joint_state->position.resize(9);
@@ -101,6 +97,8 @@ void PA10Controller::initJointState(sensor_msgs::JointState *joint_state)
     return;
 }
 
+
+// Calculate forward kinematics based on intial_angle_vector
 std::vector<std::vector<double>> PA10Controller::ForwardKinematics(std::vector<double> angle_vector)
 {
 
@@ -111,7 +109,7 @@ std::vector<std::vector<double>> PA10Controller::ForwardKinematics(std::vector<d
     std::vector<std::vector<double>> result_vector(4);
     std::vector<double> temp_row_vector(4);
 
-
+    // Initialize matrices with zeroes to avoid random values
     for (int k = 0; k < 4; ++k) {
         for (int i = 0; i < 4 ; ++i) {
             mat_a01.set_value(k, i, 0.0);
@@ -123,7 +121,7 @@ std::vector<std::vector<double>> PA10Controller::ForwardKinematics(std::vector<d
         }
     }
 
-    //fill constant values into joint matrices
+    // Fill constant values into joint matrices
     mat_a01.set_value(2, 1, -1);
     mat_a01.set_value(2, 3, 317);
     mat_a01.set_value(3, 3, 1);
@@ -146,6 +144,8 @@ std::vector<std::vector<double>> PA10Controller::ForwardKinematics(std::vector<d
     mat_a56.set_value(2, 3, 70);
     mat_a56.set_value(3, 3, 1);
 
+
+    // Convert angles from deg to rad.
     for(int i = 0; i < 7; i++){
         angle_vector[i] = angle_vector[i] * M_PI/180;
     }
@@ -157,7 +157,7 @@ std::vector<std::vector<double>> PA10Controller::ForwardKinematics(std::vector<d
     theta_vector[4] = angle_vector[5];
     theta_vector[5] = angle_vector[6];
 
-    //fill theta-dependent variables into joint matrices
+    // Fill theta-dependent variables into joint matrices
     mat_a01.set_value(0, 0, cos(theta_vector[0]));
     mat_a01.set_value(0, 2, -sin(theta_vector[0]));
     mat_a01.set_value(1, 0, sin(theta_vector[0]));
@@ -192,8 +192,9 @@ std::vector<std::vector<double>> PA10Controller::ForwardKinematics(std::vector<d
 
     transform_mat = mat_a01 * mat_a12 * mat_a23 * mat_a34 * mat_a45 * mat_a56;
 
-    transform_mat.print();
+    //transform_mat.print();
 
+    // Convert transformation matrix to vector of vector form.
     for (int j = 0; j < 4; ++j) {
         for (int i = 0; i < 4; ++i) {
             temp_row_vector[i] = transform_mat.get_value(j , i);
@@ -206,6 +207,8 @@ std::vector<std::vector<double>> PA10Controller::ForwardKinematics(std::vector<d
     return result_vector;
 }
 
+
+// Read values from transformation matrix to a vector of coordinates
 std::vector<double> PA10Controller::ForwardConverter(std::vector<std::vector<double>> transform_mat)
 {
     std::vector<double> cartesian_vector(3);
@@ -217,18 +220,21 @@ std::vector<double> PA10Controller::ForwardConverter(std::vector<std::vector<dou
     return cartesian_vector;
 }
 
+
+// Calculate inverse kinematics to find joint positions based on cartesian positions
 std::vector<double> PA10Controller::InverseKinematics(std::vector<double> end_effector_vector, std::vector<std::vector<double>> transformation_matrix, std::vector<double> cartesian_vector_0)
 {
 
     std::vector<double> result_vector(6), prev_val_vector(6);
+    Matrix inverse_temp_matrix, transform_matrix_prime, transformation_matrix_mat;
 
-    //define intermediate variables
+    // Define intermediate variables
     double a2, d1, d4, d6;
     double pxi, pyi, pzi, bxp, byp, bzp, nxp, nyp, txp, typ, inter1, inter21, inter22, inter3, inter31, theta1, theta11, theta12, theta2,
             theta21, theta22, theta3, theta31, theta32, theta4, theta5, theta6;
-
     double px, py, pz, bx, by, bz;
 
+    // Calculate intermediate values
     px = transformation_matrix[0][3] + (end_effector_vector[0] - cartesian_vector_0[0]);
     py = transformation_matrix[1][3] + (end_effector_vector[1] - cartesian_vector_0[1]);
     pz = transformation_matrix[2][3] + (end_effector_vector[2] - cartesian_vector_0[2]);
@@ -242,15 +248,13 @@ std::vector<double> PA10Controller::InverseKinematics(std::vector<double> end_ef
     d4 = 480.0;
     d6 = 70.0;
 
-    std::cout.precision(3);
-
     pxi = px - d6 * bx;
     pyi = py - d6 * by;
     pzi = pz - d6 * bz;
 
     inter1 = pow(pxi, 2) + pow(pyi, 2);
 
-    //make sure inter1 is positive and calculate theta1
+
     //theta11 = bind_angle(2 * atan2(-pxi + sqrt(inter1), pyi));
     //theta12 = bind_angle(2 * atan2(-pxi - sqrt(inter1), pyi));
 
@@ -266,9 +270,6 @@ std::vector<double> PA10Controller::InverseKinematics(std::vector<double> end_ef
     inter3 = (pxi * cos(theta1)) + (pyi * sin(theta1));
     inter31 = (pow(inter3, 2) + pow((-pzi + d1), 2) - pow(a2, 2) - pow(d4, 2)) / (2 * a2 * d4);
 
-    // calculate theta3 from acos, change to atan if necessary
-    //theta3 = acos((pow(inter3, 2) + pow((-pzi + d1), 2) - pow(a2, 2) - pow(d4, 2)) / (2 * a2 * d4));
-
     theta31 = atan2(sqrt(1 - pow(inter31, 2)), ((pow(inter3, 2) + pow((-pzi + d1), 2) - pow(a2, 2) - pow(d4, 2)) / (2 * a2 * d4)));
     theta32 = atan2(-sqrt(1 - pow(inter31, 2)), ((pow(inter3, 2) + pow((-pzi + d1), 2) - pow(a2, 2) - pow(d4, 2)) / (2 * a2 * d4)));
 
@@ -278,13 +279,9 @@ std::vector<double> PA10Controller::InverseKinematics(std::vector<double> end_ef
         theta3 = theta31;
     }
 
-
-
     inter21 = (pow(d4 * sin(theta3), 2) + pow(a2 + d4 * cos(theta3), 2) - pow(d1 - pzi, 2));
     inter22 = a2 - d1 + pzi + (d4 * cos(theta3));
 
-    //calculate theta2
-    //theta2 = -2 * atan2((((pxi > 0) ? (d4 * sin(theta3)) + sqrt(inter21) : (d4 * sin(theta3)) - sqrt(inter21))), inter22);
     theta21 = (-2 * atan2((((d4 * sin(theta3)) - sqrt(inter21))), inter22));
     theta22 = (-2 * atan2((((d4 * sin(theta3)) + sqrt(inter21))), inter22));
 
@@ -294,19 +291,57 @@ std::vector<double> PA10Controller::InverseKinematics(std::vector<double> end_ef
         theta2 = theta21;
     }
 
+    // Create inverse matrix of A1 * A2 * A3, based on manually calculated values.
+    inverse_temp_matrix.set_value(0, 0, (cos(theta2 + theta3)*cos(theta1)));
+    inverse_temp_matrix.set_value(0, 1, (cos(theta2 + theta3)*sin(theta1)));
+    inverse_temp_matrix.set_value(0, 2, (-sin(theta2 + theta3)));
+    inverse_temp_matrix.set_value(0, 3, (317*sin(theta2 + theta3) + 450*sin(theta3)));
 
-    nxp = (cos(theta1) * cos(theta2) * cos(theta3)) - (cos(theta1) * sin(theta2) * sin(theta3));
-    nyp = -sin(theta1);
+    inverse_temp_matrix.set_value(1, 0, (-sin(theta1)));
+    inverse_temp_matrix.set_value(1, 1, (cos(theta1)));
+    inverse_temp_matrix.set_value(1, 2, 0);
+    inverse_temp_matrix.set_value(1, 3, 0);
 
-    txp = (cos(theta2) * cos(theta3) * sin(theta1)) - (sin(theta1) * sin(theta2) * sin(theta3));
-    typ = cos(theta1);
+    inverse_temp_matrix.set_value(2, 0, (sin(theta2 + theta3)*cos(theta1)));
+    inverse_temp_matrix.set_value(2, 1, (sin(theta2 + theta3)*sin(theta1)));
+    inverse_temp_matrix.set_value(2, 2, (cos(theta2 + theta3)));
+    inverse_temp_matrix.set_value(2, 3, (-317*cos(theta2 + theta3) - 450*cos(theta3)));
 
-    bxp = -sin(theta2 - theta3);
-    byp = 0;
-    bzp = (cos(theta2) * cos(theta3)) - (sin(theta2) * sin(theta3));
+    inverse_temp_matrix.set_value(3, 0, 0);
+    inverse_temp_matrix.set_value(3, 1, 0);
+    inverse_temp_matrix.set_value(3, 2, 0);
+    inverse_temp_matrix.set_value(3, 3, 1);
+
+    for (int i = 0; i < 4 ; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            double val = transformation_matrix[i][j];
+            transformation_matrix_mat.set_value(i, j, val);
+
+        }
+
+    }
+
+    //inverse_temp_matrix.print();
+    //transformation_matrix_mat.print();
+
+    // Create matrix T'
+    transform_matrix_prime = inverse_temp_matrix * transformation_matrix_mat;
+
+    transform_matrix_prime.print();
+
+    // Calculate intermediate values for theta4, 5, 6 calculation.
+    nxp = transform_matrix_prime.get_value(0, 0);
+    nyp = transform_matrix_prime.get_value(1, 0);
+
+    txp = transform_matrix_prime.get_value(0, 1);
+    typ = transform_matrix_prime.get_value(1, 1);
+
+    bxp = transform_matrix_prime.get_value(0, 2);
+    byp = transform_matrix_prime.get_value(1, 2);
+    bzp = transform_matrix_prime.get_value(2, 2);
 
 
-    // calculate theta4, theta5, theta6
+    // Calculate theta4, theta5, theta6
     theta4 = atan2(byp, bxp);
 
     theta5 = atan2(((bxp * cos(theta4)) + (byp * sin(theta4))), bzp);
@@ -320,13 +355,14 @@ std::vector<double> PA10Controller::InverseKinematics(std::vector<double> end_ef
     result_vector[4] = theta5;
     result_vector[5] = theta6;
 
+    // For delta-value based movement, record previous joint angle values
     prev_val_vector = result_vector;
 
     return result_vector;
-
 }
 
 
+// Function used to limit joint angles (currently not used)
 double PA10Controller::bind_angle(double angle)
 {
     if(angle < 0) {
@@ -336,7 +372,7 @@ double PA10Controller::bind_angle(double angle)
 }
 
 
-//find coefficient values for each interpolation function
+// Find coefficient values for each interpolation function
 std::vector<double> PA10Controller::FindCoeffVector(double init_pos, double final_pos, double movement_dur)
 {
     std::vector<double> coeff_vector(6);
@@ -357,21 +393,30 @@ std::vector<double> PA10Controller::FindCoeffVector(double init_pos, double fina
     return coeff_vector;
 }
 
-double PA10Controller::findCur5thOrderVal(double ticks, double duration_ticks, double x_0, double x_f){
+
+// Find interpolated values between x_0 and x_f to use in smooth path generation
+double PA10Controller::findCur5thOrderVal(double ticks, double duration_ticks, double x_0, double x_f)
+{
 
     std::vector<double> coeff = FindCoeffVector(x_0, x_f, duration_ticks);
-    return coeff[0]+(coeff[1]*ticks)+(coeff[2]*ticks*ticks)+(coeff[3]*ticks*ticks*ticks)+(coeff[4]*ticks*ticks*ticks*ticks)+(coeff[5]*ticks*ticks*ticks*ticks*ticks);
+    //return coeff[0]+(coeff[1]*ticks)+(coeff[2]*ticks*ticks)+(coeff[3]*ticks*ticks*ticks)+(coeff[4]*ticks*ticks*ticks*ticks)+(coeff[5]*ticks*ticks*ticks*ticks*ticks);
+
+    return coeff[0]+(coeff[1] * ticks)+(coeff[2] * pow(ticks, 2))+(coeff[3] * pow(ticks, 3))+(coeff[4] * pow(ticks, 4))+(coeff[5] * pow(ticks, 5));
 }
 
 
+// Generate line path and calculate corresponding joint positions
 std::vector<double> PA10Controller::LinePathGenerator(std::vector<double> cartesian_vector_0, std::vector<double> cartesian_vector_d, double movement_dur, double ticks, std::vector<std::vector<double>> transformation_mat)
 {
     std::vector<double> via_point_vector(3);
     std::vector<double> cartesian_vector_f(6);
     std::vector<double> via_angles_vector(7);
     std::vector<double> theta_vector(6);
+
+    // Interpolate values between 0 and 1 for smooth movement
     double val5thorder = findCur5thOrderVal(ticks, movement_dur, 0, 1);
 
+    // Find final cartesian positions for each step (based on initial and delta values)
     for (int j = 0; j < 6; ++j) {
         cartesian_vector_f[j] = cartesian_vector_0[j] + cartesian_vector_d[j];
     }
@@ -391,11 +436,11 @@ std::vector<double> PA10Controller::LinePathGenerator(std::vector<double> cartes
     via_angles_vector[5] = theta_vector[4];
     via_angles_vector[6] = theta_vector[5];
 
+    // Debugging printing
     for (int i = 0; i < 6; ++i) {
         std::cout << theta_vector[i];
         //std::cout << via_point_vector[i];
         std::cout << "; ";
-
     }
     std::cout << "\n";
 
@@ -404,13 +449,42 @@ std::vector<double> PA10Controller::LinePathGenerator(std::vector<double> cartes
 }
 
 
-void PA10Controller::MoveJoint(std::vector<double> theta_coeff_matrix)
+// Generate circular path and calculate corresponding joint positions
+std::vector<double> PA10Controller::CirclePathGenerator(std::vector<double> cartesian_vector_0, std::vector<double> cartesian_vector_d, double movement_dur, double ticks, std::vector<std::vector<double>> transformation_mat)
+{
+    std::vector<double> via_point_vector(3);
+    std::vector<double> cartesian_vector_f(6);
+    std::vector<double> via_angles_vector(7);
+    std::vector<double> theta_vector(6);
+
+    // Interpolate values between 0 and 2*pi for smooth movement
+    double val5thorder = findCur5thOrderVal(ticks, movement_dur, 0, (M_PI * 2));
+
+    // Find final cartesian positions for each step (based on initial and delta values)
+    for (int j = 0; j < 6; ++j) {
+        cartesian_vector_f[j] = cartesian_vector_0[j] + cartesian_vector_d[j];
+    }
+
+//    via_point_vector[0] = cartesian_vector_0[0] + ((cartesian_vector_f[0] - cartesian_vector_0[0]) * val5thorder);
+//    via_point_vector[1] = cartesian_vector_0[1] + ((cartesian_vector_f[1] - cartesian_vector_0[1]) * val5thorder);
+//    via_point_vector[2] = cartesian_vector_0[2] + ((cartesian_vector_f[2] - cartesian_vector_0[2]) * val5thorder);
+
+    // Debugging printing
+    for (int i = 0; i < 3; ++i) {
+        //std::cout << theta_vector[i];
+        std::cout << via_point_vector[i];
+        std::cout << "; ";
+    }
+    std::cout << "\n";
+
+}
+
+void PA10Controller::MoveJoint(std::vector<double> via_angles_vector)
 {
 
     for(int i=0; i<7; ++i)
     {
-        cur_joint.position[i] = theta_coeff_matrix[i];
-
+        cur_joint.position[i] = via_angles_vector[i];
     }
 
     //publish joint states
